0bce7903eacca06ee7996d3b557fab757f67bfb7
Konstantin Popov <popov.k.g@gmail.com>
2022-06-30 23:23:13 +0300

    items 2-5 task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from "@vimeo/player";
 
+const key = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let seconds = 0;
+// const currentTimeValue = [];
+
+player.on('timeupdate', function() {
+    player.getCurrentTime().then(function (seconds) {
+
+        // currentTimeValue.push(seconds);
+        
+        // console.log(currentTimeValue);
+        // return localStorage.setItem(key, JSON.stringify(currentTimeValue));
+
+        localStorage.setItem(key, seconds);
+    });
+});
+
+// player.play('timeupdate', function() {
+//     player.setCurrentTime().then(function (seconds) {
+//         localStorage.getItem(key, seconds);
+//     });
+// });
+
+console.log(localStorage.getItem(key, seconds));
+ 
