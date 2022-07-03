250b9d5b3e76c1e345ad96290a808a564fa7cbf2
kospov <popov.k.g@gmail.com>
2022-07-03 10:50:13 +0000

    Deploying to gh-pages from @ kospov/goit-js-hw-08@874e0ed8de3c5281a8c954eedda4e28f688e694a 🚀

---
+++ 03-feedback.02b45af2.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".feedback-form"),t={};e.addEventListener("input",(function(n){t.email=e.elements.email.value,t.message=e.elements.message.value,localStorage.setItem("feedback-form-state",JSON.stringify(t)),console.dir(t)}))}();
+//# sourceMappingURL=03-feedback.02b45af2.js.map
