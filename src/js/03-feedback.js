271bbbe462da7034e70a291aaa6db304fbc0f18f
Konstantin Popov <popov.k.g@gmail.com>
2022-07-03 15:05:06 +0300

    done task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = "feedback-form-state";
@@ -15,7 +15,7 @@ feedbackForm.elements.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE
 
 feedbackForm.elements.message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
 
-feedbackForm.addEventListener('input', onFormInput);
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 
 feedbackForm.addEventListener('submit', onFormInputSubmit);
 
@@ -23,7 +23,7 @@ function onFormInput(e) {
     formInputObj.email = feedbackForm.elements.email.value;
     formInputObj.message = feedbackForm.elements.message.value;
 
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInputObj));
+   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInputObj));
 }
 
 function onFormInputSubmit(e) {
